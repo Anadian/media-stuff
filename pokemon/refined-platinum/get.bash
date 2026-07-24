c0a72d3bd3dd7c3d191d538c3e848bc79d9a706a
Anadian <willanad@yandex.com>
2026-07-24 19:03:36 -0400

    Progress

---
+++ pokemon/refined-platinum/get.bash
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cp $ANDROIDHOME/s/melonds/*.sav $ANDROIDHOME/s/melonds/*.ml[012345678] -t .
---
+++ pokemon/refined-platinum/set.bash
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cp *.sav *.ml[012345678] -t "$ANDROIDHOME/s/melonds"
